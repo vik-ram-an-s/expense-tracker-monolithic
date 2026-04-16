f571a17bff099b2b880b7a6122892a8c5fb89557
vik-ram-an-s <vikramansermakani@gmail.com>
2026-04-16 10:53:14 +0530

    config-update

--- src/main/java/com/myfin/expensetracker/ExpensetrackerApplication.java
+++ src/main/java/com/myfin/expensetracker/ExpensetrackerApplication.java
@@ -2,6 +2,7 @@ package com.myfin.expensetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.myfin.expensetracker.*")
 public class ExpensetrackerApplication {
